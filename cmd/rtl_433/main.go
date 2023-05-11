7699f80a780ae00e6e31fc03f80f4bddedca8d3e
Nate Mueller <nate@themuelle.rs>
2023-05-10 20:05:55 -0700

    Basic proxy from rtl_433 to grafana

---
+++ cmd/rtl_433/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	homemetrics "github.com/natemueller/home-metrics-collectors"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var stateHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	parts := strings.Split(msg.Topic(), "/")
+	device_id := parts[2]
+	metric := parts[3]
+	value, err := strconv.ParseFloat(string(msg.Payload()), 64)
+	if err != nil {
+		return
+	}
+
+	go func() {
+		homemetrics.SendCarbon([]string{"rtl_433", homemetrics.CleanLabel(device_id), homemetrics.CleanLabel(metric)}, value, time.Now())
+	}()
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	panic(err)
+}
+
+func collect() {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(homemetrics.Config("rtl_433.mqtt_endpoint"))
+	opts.OnConnectionLost = connectLostHandler
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Printf("unable to connect to mqtt broker: %v", token.Error())
+		return
+	}
+
+	client.Subscribe("rtl_433/devices/#", 1, stateHandler).Wait()
+
+	for {
+		time.Sleep(time.Minute)
+	}
+}
+
+func main() {
+	homemetrics.Main(collect, time.Minute)
+}
